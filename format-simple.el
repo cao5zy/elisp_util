95d8bae72cea70e9ab985cfb788f69966007d04f
caozongying <zongying_cao@163.com>
2020-08-12 15:51:54 +0800

    添加format-simple.el
    Signed-off-by: caozongying <zongying_cao@163.com>

---
+++ format-simple.el
@@ -0,0 +1,16 @@
+(defun format-simple()
+  "format the list"
+  (interactive)
+  (let ((all-str (buffer-substring-no-properties (point-min) (point-max))))
+    (let ((lines (split-string all-str " ")))
+      (goto-char (point-min))
+      (set-mark (point-max))
+      (kill-region (region-beginning) (region-end))
+      (goto-char (point-min))
+     (while (> (length lines) 0)
+       (setq line (car lines))
+       (goto-char (point-max))
+       (insert (concat "\n" line))
+       (setq lines (cdr lines))))))
+      
+(provide 'format-simple)
